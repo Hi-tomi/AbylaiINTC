0fb46a71eb283fcf7b16f220706beeb139fda430
Azhar Zhomartkyzy <90923391+Hi-tomi@users.noreply.github.com>
2024-11-15 16:19:53 +0500

    rr2

---
+++ src/main/java/com/example/abylai/AbylaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.abylai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbylaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbylaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/abylai/AbylaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abylai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbylaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
